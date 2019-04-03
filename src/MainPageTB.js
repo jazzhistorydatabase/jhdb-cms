ecd305f0b5fcf47a762dafa71b16b8e09a5438ba
matticoli <mikel.matticoli@gmail.com>
2019-04-03 00:28:15 -0400

    2-way sync with firebase for EditContributionView text fields

--- src/MainPageTB.js
+++ src/MainPageTB.js
@@ -23,8 +23,8 @@ class MainPageTB extends Component {
     //     this.props.dropbox.openFile(documentName);
     // };
   
-    handleAddButtonClick() {
-        this.props.windowSwap();
+    handleAddEditButtonClick(selectedContribution) {
+        this.props.windowSwap(selectedContribution);
     }
   
     render() {
@@ -33,15 +33,25 @@ class MainPageTB extends Component {
         return (
             <div className={" MainPage-format"}>
                 <h1>My Contributions</h1>
-                <Button onClick={this.handleAddButtonClick.bind(this)} variant="outlined" color={"primary"}
+                <Button onClick={evt => {
+                    return this.handleAddEditButtonClick.bind(this);
+                }} variant="outlined" color={"primary"}
                         className={classes.button}>Add Contribution </Button>
                 <List component="nav">
 
                     {contrib.map((e) => {
+
                         return (
                             <ListItem key={e.id || e.name}>
                                 <ListItemText primary={e.name}/>
-                                <Button variant="outlined" color={"primary"} className={classes.button}>Edit </Button>
+                                <Button variant="outlined"
+                                        color={"primary"}
+                                        className={classes.button}
+                                        onClick={evt => {
+                                            return this.handleAddEditButtonClick.bind(this)(e);
+                                        }}>
+                                    Edit
+                                </Button>
                                 <Button variant="outlined" color={"primary"}
                                         className={classes.button}>Preview </Button>
                                 <ListItemText primary={e.status}/>
