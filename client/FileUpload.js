f6308ec41740ea438a126c1102ead92fcdaa57f1
LucasNVarella <lnvarella@gmail.com>
2020-04-14 12:03:52 -0400

    Added onBlur handler to video FileUploads that converts youtubelinks into their sharable format, should the link not be a sharable link

--- client/FileUpload.js
+++ client/FileUpload.js
@@ -54,6 +54,28 @@ class FileUpload extends Component {
             }
             this.setState({fileDoc: fileDoc});
         };
+
+        this.handleLinkBLur = event => {
+            if (this.props.fileType === 'Video') {
+                let fileDoc = this.state.fileDoc;
+                let url = event.target.value;
+                if (!url.includes('youtu.be/')) { // Not a sharable link
+                    if (url.includes('youtube.com/watch?v='))  {
+                        // ...but we may be able to fix it
+                        let videoId;
+                        if (url.includes('&')) { // link includes extra params - remove them
+                            videoId = url.substring(url.indexOf("=") + 1, url.indexOf('&'));
+                        } else {
+                            videoId = url.substring(url.indexOf("=") + 1);
+                        }
+                        fileDoc.url = "https://youtu.be/" + videoId;
+                        this.setState({fileDoc: fileDoc});
+                    }
+                }
+            } else {
+                console.log("link blur handler called from non-video upload!");
+            }
+        };
     };
 
 
@@ -142,8 +164,10 @@ class FileUpload extends Component {
                     style={{margin: 5}}
                     value={(this.state.fileDoc && this.state.fileDoc[url]) || ""}
                     onChange={this.handleTextChange}
+                    onBlur={this.handleLinkBLur}
                     margin="normal"
                     variant="filled"
+                    placeholder={'Click SHARE on YouTube'}
                     InputLabelProps={{
                         shrink: true,
                     }}
