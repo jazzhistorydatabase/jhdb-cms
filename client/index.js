7ad20992f05d97a94bdb58bee2a8d418d5beab82
Mikel Matticoli <mikel.matticoli@gmail.com>
2020-05-28 19:07:56 -0400

    hmr

--- client/index.js
+++ client/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Enable parcel hot module replacement in dev
+if (module.hot) {
+    module.hot.accept()
+}
+
 if(window.location.port === "3000") {
     ReactDOM.render(<p>-  Error: Use <a href="http://localhost:8080">localhost:8080</a> to route requests through express server in dev -</p>,
             document.getElementById('root'));
